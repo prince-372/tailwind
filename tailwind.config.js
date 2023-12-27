9292566863c044dd1a309cdd51cbc837f01c71d7
prince-372 <pmor18314@gmail.com>
2023-12-27 09:35:13 +0530

    task done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      'sans': "'DM Sans', sans-serif",
+      'poppins':" 'Poppins', sans-serif",
+
+    }
+  },
+  plugins: [],
+};
